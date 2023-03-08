574e341047de9fb703e850b7eee4dcac0e674f80
Apheff <87418010+Apheff@users.noreply.github.com>
2023-03-08 13:18:34 +0100

    update!

---
+++ the-minnies/src/components/App.jsx
@@ -0,0 +1,11 @@
+import logo from '../assets/logo.svg';
+import Navbar from './Navbar';
+
+export default function App()
+{
+  return(
+    <Navbar/>
+
+  );
+}
+
---
+++ the-minnies/src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+export default function navbar()
+{
+    return(
+    <nav className="nav">
+        <a href="#">The Minnies</a>
+    </nav>
+    );  
+}
